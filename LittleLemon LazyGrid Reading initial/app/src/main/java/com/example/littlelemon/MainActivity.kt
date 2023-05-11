4a510905557fc0c39d94c409f07bf8ecce2ad9f2
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-11 11:18:36 +0530

    adding themes, and LazyGrid

---
+++ LittleLemon LazyGrid Reading initial/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.littlelemon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import com.example.littlelemon.ui.theme.LittleLemonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonTheme {
+                AppScreen()
+            }
+        }
+    }
+}
+
+@Composable
+private fun AppScreen() {
+    LazyGrid()
+}
+
+@Composable
+fun LazyGrid(){
+    LazyVerticalGrid(columns = GridCells.Adaptive(140.dp)){
+        items(1000) {
+            MyGridCell()
+        }
+    }
+}
